e7b4cdec37b1bec8c16fa8e66d061205daa3c969
Danil Rasulov <drasulovdanil@gmail.com>
2024-01-05 13:28:53 +0200

    GSD

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        var greeting = "33";
+        var greeting = "334";
 
-        greeting = "33";
+        greeting = "334";
         System.out.println(greeting);
     }
 }
