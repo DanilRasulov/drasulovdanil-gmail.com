85183d9dadcdc6e36b27c2caaa6c68206c709918
DANIL <drasulovdanil@gmail.com>
2024-01-05 18:56:38 +0200

    проверка1

--- Git Raulov/src/Main.java
+++ Git Raulov/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int a = 12;
-        double b = 8.5;
+        double b = 9.1;
         System.out.println(a + b);
     }
 }
