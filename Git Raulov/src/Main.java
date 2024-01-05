2d992619cf92e2c4fef956183e843bf21c935601
DANIL <drasulovdanil@gmail.com>
2024-01-05 17:28:01 +0200

    df

--- Git Raulov/src/Main.java
+++ Git Raulov/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        var greeting = "3434";
+        var greeting = "343456";
 
-        greeting = "3434";
+        greeting = "343456";
         System.out.println(greeting);
     }
 }
